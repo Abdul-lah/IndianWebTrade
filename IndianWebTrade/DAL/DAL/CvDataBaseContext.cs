9136e3bd52e2d58ddd03fe86bc61455f18b2477f
Abdullah <ubaids935@gmail.com>
2020-03-10 04:11:06 -0700

    dbcontext completed

---
+++ IndianWebTrade/DAL/DAL/CvDataBaseContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DAL.DAL
+{
+    public partial class CvDataBaseContext : DbContext
+    {
+        public CvDataBaseContext()
+        {
+        }
+
+        public CvDataBaseContext(DbContextOptions<CvDataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<TblMsg> TblMsg { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("workstation id=CvDataBase.mssql.somee.com;packet size=4096;user id=AbdullahCV_SQLLogin_1;pwd=xdxv95yes8;data source=CvDataBase.mssql.somee.com;persist security info=False;initial catalog=CvDataBase;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("employee");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblMsg>(entity =>
+            {
+                entity.ToTable("tbl_Msg");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Message)
+                    .IsRequired()
+                    .HasMaxLength(450);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.PhoneNo)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ IndianWebTrade/DAL/DAL/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.DAL
+{
+    public partial class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
