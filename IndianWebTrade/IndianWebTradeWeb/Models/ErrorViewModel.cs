2f39797e4c6fdb196389db2c9862b53bcba9abfb
Abdullah <ubaids935@gmail.com>
2020-03-30 12:35:25 +0530

    webProjectAdded

---
+++ IndianWebTrade/IndianWebTradeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IndianWebTradeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
